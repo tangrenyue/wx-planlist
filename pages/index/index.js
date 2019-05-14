387f3354651bf752837adfe399ff94306f3a4163
tangrenyue <724562793@qq.com>
2019-05-14 14:38:24 +0800

    calculator

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,67 +7,82 @@ const app = getApp();
 const dayText = ['日', '一', '二', '三', '四', '五', '六'];
 
 Page({
-  data: {
-    dateInfo: null, // 日期
-    pageCards: [{ // 卡片列表
-      name: 'artical',
-      text: '帖子列表',
-      icon: 'application-weather.png',
-      // icon: 'cnode-logo',
-      style: 'font-size: 80rpx; color: #026e00;',
-      desc: 'Node知识分享、行业招聘、精选热帖等，技术路上我们同在。'
-    }, {
-      name: 'square',
-      text: '趣图广场',
-      icon: 'application-joke.png',
-      style: 'color: #3c82e2;',
-      desc: '搞笑、减压沙雕图，总有一张能表达你此刻的心情。哈哈哈...'
-    },{
-      name: 'square',
-      text: '趣图广场',
-      icon: 'application-joke.png',
-      style: 'color: #3c82e2;',
-      desc: '搞笑、减压沙雕图，总有一张能表达你此刻的心情。哈哈哈...'
-    }, {
-      name: 'square',
-      text: '趣图广场',
-      icon: 'application-joke.png',
-      style: 'color: #3c82e2;',
-      desc: '搞笑、减压沙雕图，总有一张能表达你此刻的心情。哈哈哈...'
-    },{
-      name: 'square',
-      text: '趣图广场',
-      icon: 'application-joke.png',
-      style: 'color: #3c82e2;',
-      desc: '搞笑、减压沙雕图，总有一张能表达你此刻的心情。哈哈哈...'
-    }]
-  },
-  onShareAppMessage() {
-    return {
-      title: 'Node随心阅',
-      path: `/pages/index/index`,
-      imageUrl: '../../image/peach.jpeg'
-    }
-  },
-  onLoad() {
-    const current = new Date();
-    this.setData({
-      dateInfo: {
-        chineseDate: CLunar.solarToLunar(current, 'YMD'),
-        weekDay: `周${dayText[current.getDay()]}`,
-        date: util.fillZero(current.getDate())
+      data: {
+        dateInfo: null, // 日期
+        pageCards: [{ // 卡片列表
+          name: 'todos',
+          text: '帖子列表',
+          icon: 'application-weather.png',
+          // icon: 'cnode-logo',
+          style: 'font-size: 80rpx; color: #026e00;',
+          desc: 'Node知识分享、行业招聘、精选热帖等，技术路上我们同在。'
+        }, {
+          name: 'square',
+          text: '趣图广场',
+          icon: 'application-joke.png',
+          style: 'color: #3c82e2;',
+          desc: '搞笑、减压沙雕图，总有一张能表达你此刻的心情。哈哈哈...'
+        }, {
+          name: 'cal',
+          text: '计算器',
+          icon: 'application-stock.png',
+          style: 'color: #3c82e2;',
+          desc: '搞笑、减压沙雕图，总有一张能表达你此刻的心情。哈哈哈...'
+        }, {
+          name: 'square',
+          text: '趣图广场',
+          icon: 'application-joke.png',
+          style: 'color: #3c82e2;',
+          desc: '搞笑、减压沙雕图，总有一张能表达你此刻的心情。哈哈哈...'
+        }, {
+          name: 'square',
+          text: '趣图广场',
+          icon: 'application-joke.png',
+          style: 'color: #3c82e2;',
+          desc: '搞笑、减压沙雕图，总有一张能表达你此刻的心情。哈哈哈...'
+        }]
+      },
+      onShareAppMessage() {
+        return {
+          title: 'Node随心阅',
+          path: `/pages/index/index`,
+          imageUrl: '../../image/peach.jpeg'
+        }
+      },
+      onLoad() {
+        const current = new Date();
+        this.setData({
+          dateInfo: {
+            chineseDate: CLunar.solarToLunar(current, 'YMD'),
+            weekDay: `周${dayText[current.getDay()]}`,
+            date: util.fillZero(current.getDate())
+          }
+        })
+      },
+      // 跳转
+      onNavigatorTap(e) {
+        var index = e.currentTarget.dataset.index
+        var item = this.data.pageCards[index]
+        if (item.name == 'square') {
+          wx.navigateTo({
+            url: '../square/square',
+          })
+        } else if (item.name == 'todos') {
+          wx.navigateTo({
+            url: '../todos/todos',
+          })
+        } else if (item.name == 'cal') {
+          wx.navigateTo({
+            url: '../cal/cal'
+          })
+        } else if (item.name == 'joke') {
+          wx.navigateTo({
+            url: '../service/service?type=joke'
+          })
+        } else if (item.name == 'constellation') {
+          wx.navigateTo({
+            url: '../service/service?type=constellation',
+          })
+        }
       }
     })
-  },
-  // 跳转
-  gotoPage(e) {
-    const { page } = e.currentTarget.dataset;
-    let url = '/pages/index/index';
-    if (page === 'square') {
-      url = '/pages/square/square'
-    } else if (page === 'artical') {
-      url = '/pages/todos/todos'
-    }
-    wx.navigateTo({ url });
-  },
-})
